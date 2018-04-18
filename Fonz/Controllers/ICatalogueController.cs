cb01b8e9eb4ad66dbb1f109e868e8b65d3e9ab7e
Tomas <tomas@warpdevelopment.co.za>
2018-04-18 14:54:07 +0200

    #

---
+++ Fonz/Controllers/ICatalogueController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Storefront.Data;
+using Storefront.Core.Domain.Catalog;
+
+namespace Fonz.Controllers
+{
+	interface ICatalogueController
+	{
+		IDataRepository<Product> _productRepo { get; set; }
+		IDataRepository<Category> _categoryRepo { get; set; }
+	}
+}
---
+++ Fonz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Storefront.Data;
+using Storefront.Core.Domain.Catalog;
+using Autofac;
+
+namespace Fonz
+{
+	static class Program
+	{
+		private static IContainer Container { get; set; }
+
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			DependencyConfig dp = new DependencyConfig();
+
+			var builder = new ContainerBuilder();
+			//builder.RegisterType<IDataRepository<Product>>();
+			//Container = builder.Build();
+
+			dp.RegisterDependencies(builder);
+
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+	}
+}
